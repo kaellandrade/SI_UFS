38c929ad3308d2cdaf636e8cdfac0cdeaf6139a7
Micael <micael.santos@dcomp.ufs.br>
2020-10-23 08:10:39 -0300

    segunda lista

--- Estrutura/2_lista/quest1.c
+++ Estrutura/2_lista/quest1.c
@@ -1,46 +1,63 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-struct ponto
-{
-    int x;
-    int y;
-};
-
 int r(int *x, int *y);
 int b(int *x, int *y);
 int c(int *x, int *y);
-void imprimeCamp(struct ponto value);
-void lerVetor(struct ponto *p);
+void imprimeCamp(int *rafael, int *beto, int *carlos);
+void repImprimir(int *p);
 
-int main()
-{
+int main(){
     int inputs;
     scanf("%d", &inputs);
-    struct ponto vet[inputs];
-
-    for (int i = 0; i < inputs; i++)
-    {
-        scanf("%d%d", &vet[i].x, &vet[i].y);
-    }
-
+    repImprimir(&inputs);
     return 0;
 }
 
 // Função do rafael
-int r(int *x, int *y)
-{
-    return pow(3 * (*x), 2) + pow(*y, 2);
+int r(int *x, int *y){
+    return pow(3* (*x), 2) + pow(*y, 2);
 }
 
 // Função do beto
-int b(int *x, int *y)
-{
+int b(int *x, int *y){
     return 2 * pow(*x, 2) + pow(5 * (*y), 2);
 }
 
 // Função do carlos
 int c(int *x, int *y)
 {
-    return -100 * (*x) + pow(*y, 3);
+    return  pow(*y, 3) - 100 * (*x);
+}
+
+void imprimeCamp(int *r, int *b, int *c){
+    if (*r > *b && *b > *c)
+    {
+        printf("Rafael ganhou\n");
+    }
+    else if (*b > *c)
+    {
+        printf("Beto ganhou\n");
+    }
+    else
+    {
+        printf("Carlos ganhou\n");
+    }
+}
+
+void repImprimir(int *p){
+    while (*p > 0)
+    {
+        int x;
+        int y;
+
+        scanf("%d%d", &x, &y);
+
+        int rafael = r(&x, &y);
+        int beto = b(&x, &y);
+        int carlos = c(&x, &y);
+        imprimeCamp(&rafael, &beto, &carlos);
+
+        (*p)--;
+    }
 }
