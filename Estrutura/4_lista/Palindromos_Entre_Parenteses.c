2fcaa33527501830b93d0accd96bd290f677e033
Micael Andrade <micael.java@outlook.com>
2021-01-25 17:17:26 -0300

    quarta lista

---
+++ Estrutura/4_lista/Palindromos_Entre_Parenteses.c
@@ -0,0 +1,34 @@
+
+/*
+Descrição
+---------
+Escreva um algoritmo que, dada uma cadeia de caracteres, contendo parêntesis, letras maiúsculas e 
+minúsculas, mostre se os parêntesis estão balanceados e se a string formada pela união de todas as letras forma um palíndromo.
+
+
+
+Formato de entrada
+------------------
+A entrada consiste de várias linhas, 
+onde em cada linha contém uma string contendo letras maiúsculas, minúsculas e parênteses.
+
+
+Formato de saída
+-----------------
+
+Para cada string lida, seu algoritmo deverá imprimir duas linhas. 
+Uma linha com a mensagem "PARENTESES: X" e outra linha com a mensagem "PALINDROMO: Y", 
+onde X deve ser substituído por "BALANCEADOS", se os parêntesis estiverem balanceados e "NAO BALANCEADOS", caso contrário.
+Y deve ser substituído por "SIM" se a união das string formar um palíndromo e "NAO", caso contrário. 
+Estas duas linhas devem estar delimitadas por linhas contendo "----".
+
+Exemplo:
+    Entrada: (aba)
+
+    Saída:
+    ----
+    PARENTESES: BALANCEADOS
+    PALINDROMO: SIM
+    ----
+*/
+
