b82925378b98fb0e33aaedee8bddc0a9e5084cd9
kaellandrade <micael.santos@dcomp.ufs.br>
2021-05-28 16:43:05 -0300

    aplicação do algoritmo djikstra com heap

--- framworkGrafoPeso/main.py
+++ framworkGrafoPeso/main.py
@@ -1,55 +1,18 @@
-
-from __future__ import annotations
-from bellman_ford import bellman_ford
-from vertex import Vertex
-from graph import Graph
-from print_path import print_path
-from dijkstra import dijkstra
-
+#!/usr/bin/env python3
+from __future__ import annotations;
+from vertex import Vertex;
+from graph import Graph;
+from print_path import print_path;
+from dijkstra import dijkstra;
+from random_graph import random_graph;
+from datetime import datetime;
 
 if (__name__ == '__main__'):
-    cidade_grafo: Graph[Vertex] = Graph([
-        Vertex("1"),  #0
-        Vertex("2"),  #1
-        Vertex("3"),  #2
-        Vertex("4"),  #3
-        Vertex("5"),  #4
-        Vertex("6"),  #5
-        Vertex("7"),  #6
-        Vertex("8"),  #7
-        Vertex("9"),  #8
-        Vertex("10"),  #9
-        Vertex("11"),  #10
-        Vertex("12"),  #11
-        Vertex("13"),  #12
-        Vertex("14"),  #13
-        Vertex("Ourivesaria"),  #14
-    ], True)
+    tempo_inicio = datetime.now()
+    g1: Graph[Vertex] = random_graph(100)
 
-    #adiciona todos os vértives que estão em vertices
-    cidade_grafo.add_edge_by_indices(0, 12, 10);
-    cidade_grafo.add_edge_by_indices(0, 1, 8);
-    cidade_grafo.add_edge_by_indices(1, 2, 10);
-    cidade_grafo.add_edge_by_indices(2, 3, 12);
-    cidade_grafo.add_edge_by_indices(3, 7, 14);
-    cidade_grafo.add_edge_by_indices(2, 4, 9);
-    cidade_grafo.add_edge_by_indices(1, 13, 7);
-    cidade_grafo.add_edge_by_indices(4, 5, 4);
-    cidade_grafo.add_edge_by_indices(4, 6, 3);
-    cidade_grafo.add_edge_by_indices(13, 5, 8);
-    cidade_grafo.add_edge_by_indices(13, 12, 6);
-    cidade_grafo.add_edge_by_indices(6, 5, 5);
-    cidade_grafo.add_edge_by_indices(7, 6, 9);
-    cidade_grafo.add_edge_by_indices(7, 14, 10);
-    cidade_grafo.add_edge_by_indices(12, 11, 13);
-    cidade_grafo.add_edge_by_indices(11, 10, 8);
-    cidade_grafo.add_edge_by_indices(10, 9, 11);
-    cidade_grafo.add_edge_by_indices(10, 8, 16);
-    cidade_grafo.add_edge_by_indices(9, 8, 4);
-    cidade_grafo.add_edge_by_indices(8, 14, 9);
-    
-    
-    bellman_ford(cidade_grafo,0)
-    # dijkstra(cidade_grafo,0)
-    print_path(cidade_grafo.vertex_at(0), cidade_grafo.vertex_at(14))
-    # print(cidade_grafo)
+    dijkstra(g1,0) #Algoritmo Heap
+    print_path(g1.vertex_at(0), g1.vertex_at(2))
+    # print(g1)
+
+    print(datetime.now() - tempo_inicio)
--- framworkGrafoPeso/random_weight.py
+++ framworkGrafoPeso/random_weight.py
@@ -5,6 +5,3 @@ from random import randrange
 #Recebe um intervalo [A,B] e retorna um valor entre [A,B]
 def random_weight(a: int, b: int) -> int:
     return randrange(a, b + 1)
-
-
-print(random_weight(20, 100))
