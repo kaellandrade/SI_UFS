d00b0312f39fc2f27d014a5ba6769d4c3eb9dab8
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-11-16 21:17:13 -0300

    todo questão 2

--- unid_2/lista6/quest_1.py
+++ unid_2/lista6/quest_1.py
@@ -28,10 +28,10 @@ ed4 = Edificio(Point(11, 0,  'L'), 10, Point(12, 0, 'R'))
 
 edificios = [ed1, ed2, ed3, ed4]
 
-# -----------------------
+# -----------------------------------------------------
 # ed1 = Edificio(Point(4, 0, 'L'), 6, Point(8, 0, 'R'))
 # ed2 = Edificio(Point(6, 0,  'L'), 4, Point(10, 0,  'R'))
 # edificios = [ed1,ed2]
-
+# TODO: Estudar novamente esse algoritmo.
 # list(map(print, edificios))
 print(mergeEdificio(edificios))
---
+++ unid_2/lista6/quest_2.py
@@ -0,0 +1,4 @@
+# TODO: Criar função pontoDomina
+# !DICA: Pode precisar usar algoritmo de ordenação
+# !DICA: Pensar em uma solução por indução(Fraca)
+# !DICA: Talve os mergesort seja uma boa ideia
