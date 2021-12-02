1f48da939f21d23705e78cfa43a9d9ea8fbcf599
Micael Andrade <micael.santos@dcomp.ufs.br>
2021-12-01 21:05:26 -0300

    quest 1

---
+++ unid_2/lista7/quest_1.py
@@ -0,0 +1,30 @@
+comb = []
+k = 3
+b = [0]*k
+res = []
+
+
+def teste(k, i, b):
+    if(i == k):
+        res.append(b.copy())
+    else:
+        for j in range(0, 10):
+            b[i] = j
+            teste(k, i+1, b)
+
+
+def filterValisSum(l):
+    soma_pares = 0
+    soma_impares = 0
+    if(l[0] == 0):
+        return False
+    for i in range(len(l)):
+        if(i % 2 == 0):
+            soma_pares += l[i]
+        else:
+            soma_impares += l[i]
+    return soma_impares == soma_pares
+
+
+teste(k, 0, b)
+print(list(filter(filterValisSum, res)))
