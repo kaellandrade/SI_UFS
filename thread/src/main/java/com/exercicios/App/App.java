048493addecf8778ef039344c9452cc2b2cebfbe
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-03-15 16:14:57 -0300

    tstando minha classe

---
+++ thread/src/main/java/com/exercicios/App/App.java
@@ -0,0 +1,12 @@
+package com.exercicios.App;
+
+public class App {
+    public static void main(String[] args) {
+        MyArr arr = new MyArr(5);
+        
+        arr.fillVector();
+        System.out.print(arr);
+        
+    }
+
+}
