474eaa5f0a52c22d48f8c63a670ca7e3d7a8fea3
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-03-15 22:02:51 -0300

    ordenando com threads

--- thread/src/main/java/com/exercicios/App/App.java
+++ thread/src/main/java/com/exercicios/App/App.java
@@ -1,12 +1,20 @@
 package com.exercicios.App;
 
 public class App {
-    public static void main(String[] args) {
-        MyArr arr = new MyArr(5);
-        
-        arr.fillVector();
-        System.out.print(arr);
-        
+    public static void main(String[] args) throws InterruptedException {
+        MyArrThread arr = new MyArrThread(5);
+
+        arr.start();
+
+        arr.join(); // Aguardando minha Thread terminar.
+
+        System.out.println("Array Original:");
+        String originalArr = MyArrThread.formmatedArr(arr.getVector());
+        System.out.println(originalArr);
+
+        System.out.println("Array Ordenado (copia):");
+        String copyAtt = MyArrThread.formmatedArr(arr.getNewVectorSort());
+        System.out.println(copyAtt);
     }
 
 }
---
+++ thread/src/main/java/com/exercicios/App/MyArrThread.java
@@ -0,0 +1,60 @@
+package com.exercicios.App;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MyArrThread extends Thread {
+    private float[] vector;
+    private float[] newVectorSort;
+
+    public MyArrThread(int length) {
+        this.vector = new float[length];
+        this.newVectorSort = new float[length];
+    }
+
+    private void sortVector() {
+        Arrays.sort(this.newVectorSort);
+    }
+
+    public float[] getVector() {
+        return this.vector;
+    }
+
+    public float[] getNewVectorSort() {
+        return this.newVectorSort;
+    }
+
+    public void setVector(float[] vector) {
+        this.vector = vector;
+    }
+
+    /**
+     * Preeenche o vetor.
+     */
+    private void fillVector() {
+        Scanner s_vec = new Scanner(System.in);
+
+        for (int i = 0; i < this.vector.length; i++) {
+            float number = s_vec.nextFloat();
+
+            this.vector[i] = number;
+            this.newVectorSort[i] = number;
+
+        }
+        s_vec.close();
+    }
+
+    @Override
+    public void run() {
+        this.fillVector();
+        this.sortVector();
+    }
+
+    static public String formmatedArr(float[] arr) {
+        String res = "";
+        for (int i = 0; i < arr.length; i++) {
+            res += String.format("[%d] -> %.2f\n", i, arr[i]);
+        }
+        return res;
+    }
+}
