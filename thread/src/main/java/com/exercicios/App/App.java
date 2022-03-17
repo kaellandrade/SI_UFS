d38c8e994aafdc3f88070a7937ed0790392f5dd7
Micael Andrade <micael.santos@dcomp.ufs.br>
2022-03-17 15:21:06 -0300

    att 01

--- thread/src/main/java/com/exercicios/App/App.java
+++ thread/src/main/java/com/exercicios/App/App.java
@@ -1,20 +1,37 @@
 package com.exercicios.App;
 
+import java.util.Scanner;
+
 public class App {
+    private static Scanner std_answer;
+
     public static void main(String[] args) throws InterruptedException {
-        MyArrThread arr = new MyArrThread(5);
+        char answer = 'y';
+        std_answer = new Scanner(System.in);
 
-        arr.start();
+        do {
 
-        arr.join(); // Aguardando minha Thread terminar.
+            System.out.println("Digite o tamanho do vetor1: ");
+            MyArrThread arr = new MyArrThread(std_answer.nextInt());
 
-        System.out.println("Array Original:");
-        String originalArr = MyArrThread.formmatedArr(arr.getVector());
-        System.out.println(originalArr);
+            arr.start();
+    
+            arr.join(); // Aguardando minha Thread terminar.
 
-        System.out.println("Array Ordenado (copia):");
-        String copyAtt = MyArrThread.formmatedArr(arr.getNewVectorSort());
-        System.out.println(copyAtt);
+            System.out.println("Array Original:");
+            String originalArr = MyArrThread.formmatedArr(arr.getVector());
+            System.out.println(originalArr);
+
+            System.out.println("Array Ordenado (copia):");
+            String copyAtt = MyArrThread.formmatedArr(arr.getNewVectorSort());
+            System.out.println(copyAtt);
+
+            System.out.println("---Deseja repetir o processo?---");
+            answer = std_answer.next().charAt(0);
+
+        } while (answer == 'y');
+        std_answer.close();
+
     }
 
 }
---
+++ thread/src/main/java/com/exercicios/readme.md
@@ -0,0 +1,10 @@
+# Exercício 1
+
+- [X] Crie uma aplicação que faz a leitura de um vetor de inteiros
+
+- [X] Após o usuário informar todos os números, uma thread deve 
+ser criada para ordenar o vetor e exibir o vetor original e o
+vetor ordenado
+
+- [X] O usuário pode informar um novo vetor, ou seja, há uma
+estrutura de repetição no método `main()`
